ee6f641df6a0594249c30547974e3f0a73949269
JimChe <75532532+JimChe@users.noreply.github.com>
2022-05-24 22:10:17 +0800

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			permute("", "123");
+		}
+
+		static void permute(string result, string now)
+		{
+			if(now == "")
+			{
+				Console.Write(result);
+			}
+			else
+			{
+				for(int i=0; i<now.Length; i++)
+				{
+					permute(result + now[i], now.Substring(0, i) + now.Substring(i + 1));
+				}
+			}
+		}
+	}
+}
